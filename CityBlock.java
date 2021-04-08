0d84f753de8ab46c3a094033d72ad0fc9566c890
Jacob <jacobelam11@gmail.com>
2021-04-08 15:33:49 -0400

    Math Complete

--- CityBlock.java
+++ CityBlock.java
@@ -4,8 +4,7 @@ public class CityBlock implements ComputeDistance {
 
     @Override
     public double computeDistance(TrainSample Sample, TrainSample tSample) {
-        // TODO Auto-generated method stub
-        return 0;
+        return Math.abs((Sample.trainSample[0]-tSample.trainSample[0])+(Sample.trainSample[1]-tSample.trainSample[1])+(Sample.trainSample[2]-tSample.trainSample[2])+(Sample.trainSample[3]-tSample.trainSample[3]));
     }
 
 }
--- Euclidean.java
+++ Euclidean.java
@@ -4,8 +4,7 @@ public class Euclidean implements ComputeDistance {
 
     @Override
     public double computeDistance(TrainSample Sample, TrainSample tSample) {
-        // TODO Auto-generated method stub
-        return 0;
+        return Math.sqrt(Math.pow(Sample.trainSample[0]-tSample.trainSample[0],2)+Math.pow(Sample.trainSample[1]-tSample.trainSample[1], 2)+Math.pow(Sample.trainSample[2]-tSample.trainSample[2],2)+Math.pow(Sample.trainSample[3]-tSample.trainSample[3],2));
     }
 
 }
